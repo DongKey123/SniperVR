61738307f0841407ce1720f2616fc0279894f31c
PHG01 <pear0803@naver.com>
2017-11-28 20:05:10 +0900

    BloodSplatRender blood texture 랜덤 출력 삭제

--- Assets/Scripts/CameraFX/BloodSplatRender.cs
+++ Assets/Scripts/CameraFX/BloodSplatRender.cs
@@ -5,91 +5,19 @@ using UnityEngine;
 public class BloodSplatRender : MonoBehaviour
 {
 	[SerializeField]
-	Vector3 _RandomMinPos;
-
-	[SerializeField]
-	Vector3 _RandomMaxPos;
-
-	[SerializeField]
-	int		_AppearAmount;
-
-	[SerializeField]
 	float	_AppearDelay;
-
-	[SerializeField]
-	GameObject _BloodFrefab;
-
-	[SerializeField]
-	List<Texture2D> _UsedBloodTextures;
-
+	
 	bool			_play;
 	int				_objOpenIndex;
 	float			_nowTime;
 
-	Vector3			_RandomMiddlePos;
+	[SerializeField]
+	Transform _bloodSplatTransform;
+	
 	
 	// Use this for initialization
 	void Start ()
 	{
-		_RandomMiddlePos = _RandomMaxPos + _RandomMinPos;
-		_RandomMiddlePos *= 0.5f;
-
-		GameObject Blood;
-
-		Material frefabMat = _BloodFrefab.GetComponent<Renderer>().sharedMaterial;
-		Material copyMat;
-		
-		float x, y, z;
-		int tI;
-
-		List<GameObject> createBloods = new List<GameObject>();
-
-		for ( int i = 0; i < _AppearAmount; i++ )
-		{
-			Blood = Instantiate( _BloodFrefab );
-
-			x = Random.Range( _RandomMinPos.x, _RandomMaxPos.x );
-			z = Random.Range( _RandomMinPos.z, _RandomMaxPos.z );
-
-			if ( i > _AppearAmount / 2 )
-			{
-				y = Random.Range( _RandomMinPos.y, _RandomMiddlePos.y );
-			}
-			else
-			{
-				y = Random.Range( _RandomMiddlePos.y, _RandomMaxPos.y );
-			}
-
-			tI = Random.Range( 0, _UsedBloodTextures.Count );
-
-			copyMat = Instantiate( frefabMat );
-			copyMat.mainTexture = _UsedBloodTextures[tI];
-
-			Blood.GetComponent<Renderer>().sharedMaterial = copyMat;
-
-			Blood.transform.localPosition = new Vector3( x, y , Mathf.Round( z ));
-			Blood.SetActive( false );
-
-			createBloods.Add( Blood );
-		}
-
-		createBloods.Sort( delegate ( GameObject a, GameObject b )
-		{
-			if ( a.transform.localPosition.z < b.transform.localPosition.z )
-				return 1;
-			else if ( a.transform.localPosition.z > b.transform.localPosition.z )
-				return -1;
-			return 0;
-		});
-
-		Vector3 lpos;
-		foreach ( GameObject blood in createBloods )
-		{
-			lpos = blood.transform.localPosition;
-			blood.transform.parent = transform;
-			blood.transform.localPosition = lpos;
-		} 
-			
 		_play = false;
 		_objOpenIndex = 0;
 		_nowTime = 0;
@@ -100,7 +28,7 @@ public class BloodSplatRender : MonoBehaviour
 		if ( _play == false )
 			return;
 
-		if ( _objOpenIndex >= transform.childCount )
+		if ( _objOpenIndex >= _bloodSplatTransform.childCount )
 			return;
 
 		_nowTime += Time.deltaTime;
@@ -108,7 +36,7 @@ public class BloodSplatRender : MonoBehaviour
 		if ( _nowTime >= _AppearDelay )
 		{
 			_nowTime -= _AppearDelay;
-			transform.GetChild( _objOpenIndex ).gameObject.SetActive( true );
+			_bloodSplatTransform.GetChild( _objOpenIndex ).gameObject.SetActive( true );
 			_objOpenIndex++;
 		}
 
